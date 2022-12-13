59e260eb67d42ac397a3d493900d3ab4587bdad6
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-12 17:27:14 -0800

    Lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* Reverse alphabet */
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

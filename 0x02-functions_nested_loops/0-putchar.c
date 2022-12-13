cf3a33c6bffda2f57e63d193d476d560a0afa868
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-13 09:03:33 -0800

    Print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char value[] = "_putchar";
+	int i;
+
+	for(i = 0; i < strlen(value); i++) {
+		putchar(value[i]);
+	}
+	putchar('\n');
+	return (0);
+}

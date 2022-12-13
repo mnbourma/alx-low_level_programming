65834281ea98adf431da20b739d865f2a48eff65
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-13 10:27:57 -0800

    Print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(value[i]);
+	}
+	putchar('\n');
+}

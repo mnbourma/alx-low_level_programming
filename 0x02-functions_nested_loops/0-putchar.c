185da538d04b0c5296ad929ee32bef5161ffd13a
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-13 09:16:09 -0800

    Print

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include "main.h"
 /**
  *  main - Entry point
  *
  *  Return: Always 0 (Success)
  */
-int main(void)
+int putchar(void)
 {
 	char value[] = "_putchar";
 	int i;

2d27edf23a83f84387b1deabd5ba281138204b09
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-12 16:42:43 -0800

    Reverse Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/* Reverse alphabet */
+/**
+ * main - Entry point
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	char base16[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 25; i = 0; i--)
+	{
+		putchar(base16[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

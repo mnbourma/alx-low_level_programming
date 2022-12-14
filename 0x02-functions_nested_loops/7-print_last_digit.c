ddf3cc1c49adb596f82b0f0402ed1d54f8065c72
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-14 22:25:29 +0000

    Print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,8 @@ int print_last_digit(int c)
 {
 	char n[50];
 	int last_digit;
-	int size = sizeof(n) / sizeof(n[0]);
 
 	n[50] = c + '0';
-	last_digit = n[size - 1];
+	last_digit = n[sizeof(n) / sizeof(n[0]) - 1];
 	return (last_digit);
 }

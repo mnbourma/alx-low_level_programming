ae108849859a44e80714b58b439c3d1e14cd68d3
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-14 16:36:10 +0000

    Check if lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _islower - checks if a char is lowercase
+ *
+ * Return: 1 if lowercase and 0 otherwise
+ */
+int _islower(char c)
+{
+	if (islower != 0)
+	{
+		return (1);
+	}
+	return (0);
+}

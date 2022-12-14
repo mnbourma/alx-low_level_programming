9a01e0d5f14a81d2987580c56c3da0fe71a82b9b
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-14 21:41:57 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @c: integer to pass to _islower
+ *
+ * Return: the abs
+ */
+int _abs(int c)
+{
+	return (abs(c));
+}

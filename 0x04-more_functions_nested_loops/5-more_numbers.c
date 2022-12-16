9071cf57db87180669ac324bc5e20dee9f967855
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-16 18:20:11 +0000

    Print a line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,26 +8,23 @@ void more_numbers(void)
 {
 	int i;
    	int j;
+	int k;
     	char c[2];
 
     	for (i = 0; i < 10; i++)
     	{
         	for (j = 0; j < 15; j++)
         	{
+			c[0] = j + '0';
             		if (j > 9)
            		{
                			c[0] = 1 + '0';
                 		c[1] = (j - 10) + '0';
             		}
-           		else
-            		{
-                		c[0] = j + '0';
-            		}	
-            		putchar(c[0]);
-            		if (j > 9)
-            		{
-                		putchar(c[1]);
-            		}
+			for (k = 0; k < strlen(c); k++)
+			{
+				putchar(c[k]);
+			}
         	}
         	if (i < 9)
         	{

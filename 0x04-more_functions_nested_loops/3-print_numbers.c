19920339d512feb3c75e9e4c2e1d806beb57aa2e
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-16 14:22:21 +0000

    Print 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}

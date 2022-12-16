5821788c1911509359d6bc4dcd1a270ac6802f71
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-16 22:38:19 +0000

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* print_most_numbers - Prints the numbers, from 0 to 9, followed by a new line - do not print 2 and 4 */
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9, followed by a new line - do not print 2 and 4 
+ **/
 void print_most_numbers(void)
 {
 	int i;

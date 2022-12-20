7bf0f479f798fbb49b9a467b84f8bf121937701b
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-20 17:29:28 +0000

    Print

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - prints a string, followed by a new line
+ * @s: the given string
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; i < (int)strlen(s); i += 2)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}

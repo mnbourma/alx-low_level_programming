3c7184dd721b068aae2f2012c47ef55fb1faac37
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-20 20:43:16 +0000

    Copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copies the string pointed including the null byte
+ * @dest: the destination
+ * @src: source
+ */
+char _strcpy(char *dest, char *src)
+{
+	int i;
+	char cp[(int)strlen(dest + 1)] = src;
+	return (cp);
+}

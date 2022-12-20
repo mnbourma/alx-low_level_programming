b8b05edebb5743578ea5a915bacd357dbc6cecf7
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-20 20:14:40 +0000

    Print n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - prints n elements of an array
+ * @s: the given string
+ */
+void print_array(int *a, int n);
+{
+	int i, j;
+	char s[50];
+
+	for (i = 0; i < n; i ++)
+	{	
+		sprintf(s,  "%d, ", a[i]);
+		for (j = 0; j < strlen(s) - 2; j++)
+		{
+			putchar(s[j]);
+		}
+		while (i < n - 1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}

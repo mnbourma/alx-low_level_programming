bf0f220a4af8858ba9c3cfa5f75447daf1fd0454
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-23 15:28:05 +0000

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,39 @@
 #include <stdio.h>
 /**
- * more_numbers - Prints 0 to 14 ten times
+ * main - prints 1 to 100
  **/
-void more_numbers(void)
+void main(void)
 {
 	int i, j;
-    	char c1, c2;
 
-    	for (i = 0; i < 10; i++)
-    	{
-        	for (j = 0; j < 15; j++)
-        	{
-			c1 = j + '0';
-            		if (j > 9)
-           		{
-               			c1 = 1 + '0';
-                		c2 = (j - 10) + '0';
-            		}
-			putchar(c1);
-			if (j > 9)
-			{
-				putchar(c2);
-			}
-        	}
-        	if (i < 9)
-        	{
-            		putchar('\n');
-        	}
-    	}
+	j = 0;
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+		}
+		else
+		{
+			putchar((i % 10) + '0');
+			if (i > 9)
+				putchar(j + '0');
+		}
+		if (j == 9)
+			j = 0;
+		if (i < 99)
+			putchar(' ');
+		j++;
+	}
+	putchar('\n');
 }

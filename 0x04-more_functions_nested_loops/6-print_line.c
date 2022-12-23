1b602ab3dfce8e3df757a045321a7e09bbe445f0
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-23 14:25:51 +0000

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
- * print_line - prints a line of n length
- *
- * @n: line length
+ * print_line - prints a line of length n.
+ * @n: line length.
  * */
-
 void print_line(int n)
 {
 	int i;
@@ -13,9 +10,7 @@ void print_line(int n)
 	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
-		{
 			putchar('_');
-		}
 	}
 	putchar('\n');
 }

1d7b844384b319c33c74769b2c70a3a6fc15dced
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-23 14:36:15 +0000

    Print a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
-int _putchar(char c);
-
 /**
  * print_square - prints a square made of #
- *
  * @size: size of the square
- **/
-
+ */
 void print_square(int size)
 {
 	int i, j;
-	
+
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');
+				putchar('#');
 			}
 			if (i < size - 1)
 			{
-				_putchar('\n');
+				putchar('\n');
 			}
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }

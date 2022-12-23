4f9d1bc3d5fec477c8d3e3d2b805e9d2e3ffa05d
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-23 14:51:07 +0000

    Print 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,27 +5,27 @@
 void more_numbers(void)
 {
 	int i, j;
-    	char c1, c2;
+	char c1, c2;
 
-    	for (i = 0; i < 10; i++)
-    	{
-        	for (j = 0; j < 15; j++)
-        	{
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
 			c1 = j + '0';
-            		if (j > 9)
-           		{
-               			c1 = 1 + '0';
-                		c2 = (j - 10) + '0';
-            		}
+			if (j > 9)
+			{
+				c1 = 1 + '0';
+				c2 = (j - 10) + '0';
+			}
 			putchar(c1);
 			if (j > 9)
 			{
 				putchar(c2);
 			}
-        	}
-        	if (i < 9)
-        	{
-            		putchar('\n');
-        	}
-    	}
+		}
+		if (i < 9)
+		{
+			putchar('\n');
+		}
+	}
 }

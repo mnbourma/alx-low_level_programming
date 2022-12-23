50c9bcfa8fa92f027aa9ee3c9586d181e74bbbdd
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-23 16:11:38 +0000

    Print 1 to 14 ten times

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
 /**
- * main - prints 1 to 100
- **/
+ * main - prints 1 tot 14 ten times
+ */
+void fizz(void);
+void buzz(void);
 void main(void)
 {
-	int i, j;
+	int i, j, k;
 
-	j = 0;
 	for (i = 1; i < 101; i++)
 	{
+		if (i % 10 == 0)
+		{
+			j = i / 10;
+			k = 0;
+		}
 		if (i % 3 == 0)
 		{
-			putchar('F');
-			putchar('i');
-			putchar('z');
-			putchar('z');
+			fizz();
 		}
 		else if (i % 5 == 0)
 		{
-			putchar('B');
-			putchar('u');
-			putchar('z');
-			putchar('z');
+			buzz();
 		}
 		else
 		{
-			putchar((i % 10) + '0');
 			if (i > 9)
+			{
 				putchar(j + '0');
+				putchar(k + '0');
+			}
+			else
+			{
+				putchar(i + '0');
+			}
 		}
-		if (j == 9)
-			j = 0;
+		if (i > 9)
+		{
+			k++;
+		}
 		if (i < 99)
+		{
 			putchar(' ');
-		j++;
+		}
 	}
 	putchar('\n');
+}
+void fizz(void)
+{
+	putchar('F');
+	putchar('i');
+	putchar('z');
+	putchar('z');
+}
+void buzz(void)
+{
+	putchar('B');
+	putchar('u');
+	putchar('z');
+	putchar('z');
 }

43055b2fff44f14b96e8e7ce77803ccaf5458962
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-24 20:59:29 +0000

    Print the last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* Print the last digit of a number */
 /**
- * main - Entry point
- *
+ * main - entry point
+ * Description: prints the last digit of a number
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int n;
+	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char digits[] = "" + n + "";
-
+	last_digit = n % 10;
+	putchar(last_digit + '0');
 	return (0);
 }

c25aa1c1e04b69b7bee728e4c96ca0372fb728e2
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-24 21:53:11 +0000

    Prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,11 +18,11 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-			}
-			if (i < 8)
-			{
-				putchar(',');
-				putchar(' ');
+				if (i < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}

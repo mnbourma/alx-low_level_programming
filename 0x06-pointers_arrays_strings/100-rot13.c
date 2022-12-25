1b01d9f8a97403d4177e0c213ab600916e884ce6
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-25 09:35:50 +0000

    Encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ */
+char *rot13(char *s)
+{
+	int i, j, index;
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < (int)strlen(s); i++)
+	{
+		for (j = 0; j < (int)strlen(a); j++)
+		{
+			index = (j + 13) % 26;
+
+			if (toupper(s[i]) == toupper(a[j]))
+			{
+				s[i] = a[index];
+			}
+		}
+	}
+	s[0] = toupper(s[0]);
+	s[1] = toupper(s[1]);
+	s[2] = toupper(s[2]);
+
+	return (s);
+}

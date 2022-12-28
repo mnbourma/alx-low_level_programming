035d12d5bd2553fd3ca49c806ea98b9fd6e7a141
Mahamat Nour Bourma <mnbourma@outlook.com>
2022-12-28 17:45:11 +0000

    Print buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * print_buffer - prints a buffer
+ * Description: prints a buffer
+ * @b: the buffer
+ * @size: buffer size
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size == 0)
+	{
+		putchar('\n');
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; i += 2)
+		{
+			printf("%02x%02x ", b[j], b[j + 2]);
+		}
+		for (j = i; j < i + 10; j++)
+		{
+			putchar(b[j]);
+		}
+		putchar('\n');
+	}
+}

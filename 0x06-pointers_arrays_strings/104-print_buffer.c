72c071a5a3f99d17dd452aac2ea84662566f2964
mnbourma <mnbourma@outlook.com>
2022-12-30 20:23:20 +0000

    Buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <string.h>
+void print_10(char*, int, int);
 /**
  * print_buffer - prints a buffer
  * Description: prints a buffer
@@ -9,22 +9,64 @@
 void print_buffer(char *b, int size)
 {
 	int i, j;
-
-	if (size == 0)
-	{
-		putchar('\n');
-	}
+	
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
 		for (j = i; j < i + 10; i += 2)
 		{
-			printf("%02x%02x ", b[j], b[j + 1]);
+			if (j < size)
+			{
+				printf("%02x", b[j]);
+			}
+			else
+			{
+				printf("  ");
+			}
+			if (j + 1 < size)
+			{
+				printf("%02x ", b[j + 1]);
+			}
+			else
+			{
+				printf("   ");
+			}
+			if (j % 10 == 8)
+			{
+				break;
+			}
 		}
-		for (j = i; j < i + 10; j++)
-		{
-			putchar(b[j]);
-		}
+		print_10(b, i, size);
 		putchar('\n');
+	}
+}
+/**
+ * print_10 - prints the content of the buffer
+ * Description: prints in ten bytes the content of a buffer
+ * @b: the given buffer
+ * @index: index of the loop
+ * @size: buffer's size
+ */
+void print_10(char *b, int index, int size)
+{
+	int j;
+	
+	for (j = index; j < index + 10; j++)
+	{
+		if (j < size)
+		{
+			if (isprint(b[j]))
+			{
+				putchar(b[j]);
+			}
+			else
+			{
+				putchar('.');
+			}
+		}
+		if (j % 10 == 9)
+		{
+			break;
+		}
 	}
 }

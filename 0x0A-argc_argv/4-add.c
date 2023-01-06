30fba00df7c3ac5ad2613e8ebbef8e7f9ad1511b
mnbourma <mnbourma@outlook.com>
2023-01-06 23:53:14 +0000

    Program that adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
+			if (isdigit(*argv[i]) && (atof(argv[i]) - atoi(argv[i])) == 0)
 			{
 				sum += atoi(argv[i]);
 			}

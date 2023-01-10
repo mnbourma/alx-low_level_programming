15d4fc10bc819560ddc0fc8cb844bbf882904d40
mnbourma <mnbourma@outlook.com>
2023-01-10 20:40:18 +0000

    Function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,12 +6,12 @@
  * @s2: this is the second string.
  * Return: a pointer to the allocated memory, or NULL.
  */
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
 	if (s2 == NULL)
 	{
 		return (NULL);
 	}
 	strcat(s1, s2);
-	return (s);
+	return (s1);
 }

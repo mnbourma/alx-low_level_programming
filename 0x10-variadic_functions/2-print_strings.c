c11d90c3801e7deda307cb961b352eb4d9a60bd1
Mahamat Nour Bourma <bourmamn0gmail.com>
2023-01-25 21:48:48 +0000

    Function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: this is the string to be printed between numbers.
+ * @n: this is the number of integers passed to the function.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list vars;
+	unsigned int i;
+
+	va_start(vars, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(vars, ));
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(vars);
+}
